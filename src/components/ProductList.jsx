33b680e310278d4b1e0cb2d7c279c43133048669
Himanshu Kapoor <himanshukapoor898@gmail.com>
2024-01-09 14:59:52 +0530

    API called and data displayed in tabular format

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -25,28 +25,10 @@ const ProductList = () => {
     return <div>Error: {error.message}</div>;
   }
 
-//   const columns = [
-//     { id: '1', label: 'Id' },
-//     { id: '2', label: 'Sub Category' },
-//     { id: '3', label: 'Title' },
-//     { id: '4', label: 'Price' },
-//     { id: '5', label: 'Popularity' },
-//   ]
-  //Display 
   return (
     <div>
       <h1>Product List</h1>
       {data && data.count > 0 && (
-        // <ul>
-        //   {Object.keys(data.products).map((productId) => {
-        //     const product = data.products[productId];
-        //     return (
-        //       <li key={productId}>
-        //         <strong>{product.title}</strong> - {product.price} - Popularity: {product.popularity}
-        //       </li>
-        //     );
-        //   })}
-        // </ul>
         <TableContainer data = {data}/>
       )}
     </div>
